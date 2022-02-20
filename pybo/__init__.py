c301abcdb236ac3c2bfef313fa8e11a9ebcd96ce
Indian966 <rlaalsrb4175@gmail.com>
2022-02-20 18:18:45 +0900

    moved directory

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     migrate.init_app(app, db)
 
     # 블루프린트
-    from .views import main_views
+    from pybo.views import main_views
     app.register_blueprint(main_views.bp)
 
     return app
